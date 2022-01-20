77a19a74e5440abe9f993bd607544dc180b6c2d5
Olga Antushevich <antushevich@gmail.com>
2022-01-20 23:00:18 +0200

    feat: add function decode

--- src/index.js
+++ src/index.js
@@ -1,45 +1,71 @@
 const MORSE_TABLE = {
-    '.-':     'a',
-    '-...':   'b',
-    '-.-.':   'c',
-    '-..':    'd',
-    '.':      'e',
-    '..-.':   'f',
-    '--.':    'g',
-    '....':   'h',
-    '..':     'i',
-    '.---':   'j',
-    '-.-':    'k',
-    '.-..':   'l',
-    '--':     'm',
-    '-.':     'n',
-    '---':    'o',
-    '.--.':   'p',
-    '--.-':   'q',
-    '.-.':    'r',
-    '...':    's',
-    '-':      't',
-    '..-':    'u',
-    '...-':   'v',
-    '.--':    'w',
-    '-..-':   'x',
-    '-.--':   'y',
-    '--..':   'z',
-    '.----':  '1',
-    '..---':  '2',
-    '...--':  '3',
-    '....-':  '4',
-    '.....':  '5',
-    '-....':  '6',
-    '--...':  '7',
-    '---..':  '8',
-    '----.':  '9',
-    '-----':  '0',
+    '.-': 'a',
+    '-...': 'b',
+    '-.-.': 'c',
+    '-..': 'd',
+    '.': 'e',
+    '..-.': 'f',
+    '--.': 'g',
+    '....': 'h',
+    '..': 'i',
+    '.---': 'j',
+    '-.-': 'k',
+    '.-..': 'l',
+    '--': 'm',
+    '-.': 'n',
+    '---': 'o',
+    '.--.': 'p',
+    '--.-': 'q',
+    '.-.': 'r',
+    '...': 's',
+    '-': 't',
+    '..-': 'u',
+    '...-': 'v',
+    '.--': 'w',
+    '-..-': 'x',
+    '-.--': 'y',
+    '--..': 'z',
+    '.----': '1',
+    '..---': '2',
+    '...--': '3',
+    '....-': '4',
+    '.....': '5',
+    '-....': '6',
+    '--...': '7',
+    '---..': '8',
+    '----.': '9',
+    '-----': '0',
 };
 
 function decode(expr) {
-    // write your solution here
-}
+
+    let numbersArray = [];
+    expr = expr.split('');
+
+    while (expr.length !== 0) {
+        let byte = expr.splice(0, 10);
+        numbersArray.push(byte.join(''));
+    };
+
+    let symbolsArray = numbersArray.map((string) => {
+        string = string.replace(/10/g, '.');
+        string = string.replace(/11/g, '-');
+        string = string.replace(/0/g, '');
+        return string;
+    });
+
+    let encodedString = '';
+
+    for (let item of symbolsArray) {
+        if (item.includes("*")) {
+            encodedString += " ";
+        } else {
+            encodedString += MORSE_TABLE[item];
+        };
+    };
+
+    return encodedString;
+};
 
 module.exports = {
     decode
